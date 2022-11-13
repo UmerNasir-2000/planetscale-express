bf6ff60b74f8711043328e12e2fa764796d90321
Umer Nasir <unasir@dawaai.pk>
2022-11-13 11:57:07 +0500

    Added logging.

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ dotenv.config();
 
 import 'reflect-metadata';
 import express from 'express';
-import { AppDataSource } from './data-source';
-import artistsRouter from './routes/artists.route';
+import morgan from 'morgan'
 import { StatusCodes } from 'http-status-codes';
+
+import { AppDataSource } from './data-source';
+import logger from './utils/logger'
+import artistsRouter from './routes/artists.route';
 
 async function main() {
 
@@ -17,22 +20,26 @@ async function main() {
 
         const { isInitialized } = await AppDataSource.initialize();
 
-        if (isInitialized) console.log('Successfully connected to planetscale..')
+        if (isInitialized) logger.info('Successfully connected to planetscale..');
 
-        app.get('/healthcheck', (request, response) => response.status(StatusCodes.OK).json({ message: 'ping successfull!' }))
+        app.use(morgan('tiny'));
 
-        app.use('/api/artists', artistsRouter);
+        app.get('/healthcheck', (request, response) => response.status(StatusCodes.OK).json({ message: 'ping successfull!' }));
 
-        app.use('', (request, response) => { 
+        app.use('/v1/artists', artistsRouter);
+
+        app.use('', (_, response) => { 
             return response.status(StatusCodes.NOT_FOUND).json({ message: 'route does not exists.' });
         })
 
-        app.listen(PORT, () => console.log(`Server running at https://yts-api.cyclic.app/:${PORT}`));
+        app.listen(PORT, () => logger.info(`Server running at https://yts-api.cyclic.app:${PORT}/`));
 
 	
     } catch (error) {
 
-        console.log("🚀 ~ error", error)
+        logger.error("error while starting app", error);
+
+        process.exit(1);
 	
     }
    
